a29378e1df3c6c55d00ecf9e3ad10ec25832cf40
Lia Amalia <lia1128@icloud.com>
2019-12-23 16:50:20 -0600

    updating file

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ var connection;
 // var connection = mysql.createConnection({ // Set connection parameters
 //   host: "localhost",
 //   user: "root",
-//   password: "0118488320613",
+//   password: "password",
 //   database: "burgers_db"
 // });
 
